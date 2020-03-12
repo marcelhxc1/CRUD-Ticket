6cb5225511461e57c16a3f93a7ba6aa4fa80a741
Marcel Bueno <marcelhxc@gmail.com>
2020-03-12 17:56:40 -0300

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
     <?php if (isLoggedIn()): ?>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand active" href="#">Olá, <?php echo $_SESSION['user_name']; ?></a>
+            <a class="navbar-brand active" href="#">Olá, <?= $_SESSION['user_name']; ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
