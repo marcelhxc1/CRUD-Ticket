3c9f9f904fff6e5e929c34f1bcc8d73fbed831a5
Marcel Bueno <marcelhxc@gmail.com>
2020-03-12 15:11:38 -0300

    Update

---
+++ includes/nav.php
@@ -0,0 +1,18 @@
+    <?php if (isLoggedIn()): ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand active" href="#">Olá, <?php echo $_SESSION['user_name']; ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Listar Tickets</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="logout.php">Sair</a></a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    <?php endif; ?>
--- tickets.php
+++ tickets.php
@@ -5,72 +5,43 @@ include './includes/header.php';
 require './conn/conn.php';
 
 ?>
-    <?php if (isLoggedIn()): ?>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand active" href="#">Olá, <?php echo $_SESSION['user_name']; ?></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Listar Tickets</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="logout.php">Sair</a></a>
-                </li>
-                </ul>
+    <?php include './includes/nav.php' ?>
+      <div class="container">
+        <div class="row text-center">
+          <h1 class="text-center h12">Abra seu chamado!</h1>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row formu2">
+          <form method="post" action="inserir.php">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="titulo">Titulo</label>
+                <input type="text" class="form-control" id="titulo" placeholder="Seu titulo aqui">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="descricao">Descrição do Problema</label>
+                <textarea class="form-control" id="texto" rows="3" placeholder="Descreva seu problema aqui."></textarea>
+              </div>
             </div>
-        </nav>
-    <?php endif; ?>
-
-
-        <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+            <div class="form-group">
+              <label for="data">Data</label>
+              <input type="text" class="form-control" id="data" readonly=“true” value="<?= date("d/m/Y"); ?>">
+            </div>
+            <div class="form-group">
+              <label for="prioridade">Prioridade</label>
+              <select id="prioridade" class="form-control">
+                  <option selected>Escolha a prioridade</option>
+                  <option value="Alta">Alta</option>
+                  <option value="Média">Média</option>
+                  <option value="Baixa">Baixa</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Abrir chamado</button>
+          </form>
+        </div>
+      </div>
+        
 
 <?php 
 
